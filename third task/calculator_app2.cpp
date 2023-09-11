9036eade2a50dc86603ea359a9f7951623d6077d
CALLISTUS <callistus406@gmail.com>
2023-09-11 06:30:32 -0400

    updated

--- third task/calculator_app2.cpp
+++ third task/calculator_app2.cpp
@@ -2,67 +2,73 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 
-
-
-	
 	char response;
 	double num1, num2, result;
 
-	while (true) {
+	while (true)
+	{
 		cout << "**This is a basic calculator**" << endl;
 
-	cout << " a. Add 2 numbers\n b. subtract 2 numbers \n"
-			" c. Multiply 2 numbers \n"
-			" d. Divide 2 numbers \n"
-			" e exit" << endl;
-	cout << "\n\n";
-	cout << "Enter your option:" << endl;
-	cin >> response;
-	if (response >= 'a' && response < 'e') {
+		cout << " a. Add 2 numbers\n b. subtract 2 numbers \n"
+				" c. Multiply 2 numbers \n"
+				" d. Divide 2 numbers \n"
+				" e exit"
+			 << endl;
+		cout << "\n\n";
+		cout << "Enter your option:" << endl;
+		cin >> response;
+		if (response >= 'a' && response < 'e')
+		{
 
-		cout << "Enter two numbers" << endl;
-		cin >> num1 >> num2;
-		switch (response) {
+			cout << "Enter two numbers" << endl;
+			cin >> num1 >> num2;
+			switch (response)
+			{
 
-		case 'a':
+			case 'a':
 
-			result = num1 + num2;
+				result = num1 + num2;
 				cout << "Output:" << result << endl;
-			break;
-		case 'b':
-			result = num1 - num2;
+				break;
+			case 'b':
+				result = num1 - num2;
 				cout << "Output:" << result << endl;
-			break;
-		case 'c':
-			result = num1 * num2;
+				break;
+			case 'c':
+				result = num1 * num2;
 				cout << "Output:" << result << endl;
-			break;
+				break;
 
-		case 'd':
-				if (num2 == 0) {
-					cout << "\nCannot divide " << num1 << "  By Zero\n" << endl;
-
-			} else {
-				result = num1 / num2;
+			case 'd':
+				if (num2 == 0)
+				{
+					cout << "\nCannot divide " << num1 << "  By Zero\n"
+						 << endl;
+				}
+				else
+				{
+					result = num1 / num2;
 					cout << "Output:" << result << endl;
 					cout << endl;
+					break;
+				}
 				break;
+			default:
+				exit(0);
 			}
-			break;
-		default:
+		}
+		else if (response == 'e')
+		{
+
 			exit(0);
 		}
-
-	} else if (response == 'e') {
-
-		exit(0);
-	} else {
+		else
+		{
 			cout << "\nInvalid input\n\n";
-	}
-
-
+		}
 	}
 	return 0;
 }
